f13e0a4f6ba3c9ee4329188bb4e06c326162a1c2
zfima <zzfima@gmail.com>
2020-12-23 13:29:48 +0200

    add polynomial n=2

--- Mathematics and Python for Data Analysis/Week2/assignment_2.py
+++ Mathematics and Python for Data Analysis/Week2/assignment_2.py
@@ -10,12 +10,6 @@ def f(x):
     return math.sin(x / 5) * math.exp(x / 10) + 5 * math.exp(-x / 2)
 
 
-# Show function graph [1, 15]
-x = np.arange(1, 16)
-y = map(f, x)
-pylab.plot(x, list(y))
-pylab.show()
-
 # Polynomial definition:
 # f(x) = W0 + (W1 * x^1) + (W2 * x^2) +...+(Wn * x^n)
 # First degree polynomials (linear polynomial):
@@ -29,24 +23,42 @@ f_1 = f(1)
 f_15 = f(15)
 x = np.array([[1, 1], [1, 15]])
 b = np.array([f_1, f_15])
-w = solve(x, b)
+w_1 = solve(x, b)
 
 
-def func_polynomial(x):
-    return w[0] + w[1] * x
+def func_polynomial_n_1(var_x):
+    return w_1[0] + w_1[1] * var_x
 
 
-# show it on graph
+# calculate polynomial n=2
+# form:
+# f(x) = W0 + W1 * x + W2 * x^2
+# our example:
+# f_1 = W0 + W1    + W2
+# f_8 = W0 + W1*8  + W2*64
+# f_15= W0 + W1*15 + W2*225
+f_1 = f(1)
+f_8 = f(8)
+f_15 = f(15)
+
+x = np.array([[1, 1, 1], [1, 8, 64], [1, 15, 225]])
+y = np.array([f_1, f_8, f_15])
+w_2 = solve(x, y)
+
+
+def func_polynomial_n_2(var_x):
+    return w_2[0] + w_2[1] * var_x + w_2[2] * math.pow(var_x, 2)
+
+
+# Show on graph f, n=1, n=2
 x = np.arange(16)
-y = map(func_polynomial, x)
+y = map(func_polynomial_n_2, x)
 pylab.plot(x, list(y))
-pylab.show()
 
-# show together : Investigation function and polynomial.
-# there no too much correlation
-x = np.arange(16)
-y = map(func_polynomial, x)
+y = map(func_polynomial_n_1, x)
 pylab.plot(x, list(y))
+
 y = map(f, x)
 pylab.plot(x, list(y))
+
 pylab.show()
