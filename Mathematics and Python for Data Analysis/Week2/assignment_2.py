3d3608518c2d96ac7674159afa11e8d0bc83548d
zfima <zzfima@gmail.com>
2020-12-23 09:02:03 +0200

    assignment 2: show graph of investigated function and graph of linear polynomial, based on values x = 1, x = 15 on investigated function

---
+++ Mathematics and Python for Data Analysis/Week2/assignment_2.py
@@ -0,0 +1,43 @@
+from matplotlib import pylab
+import pandas as pd
+import numpy as np
+import math
+from scipy.linalg import solve
+
+
+# Investigation function
+def f(x):
+    return math.sin(x / 5) * math.exp(x / 10) + 5 * math.exp(-x / 2)
+
+
+# Show function graph [1, 15]
+x = np.arange(1, 16)
+y = map(f, x)
+pylab.plot(x, list(y))
+pylab.show()
+
+# Polinomial defenition:
+# f(x) = W0 + (W1 * x^1) + (W2 * x^2) +...+(Wn * x^n)
+# First degree polynomials (linear polynomial):
+# f(x) = W0 + W1 * x
+# Create linear polynomial, which based on values in x = 1 and x = 15
+f_1 = f(1)
+f_15 = f(15)
+x = np.array([[1, 1], [1, 15]])
+b = np.array([f_1, f_15])
+w = solve(x, b)
+
+# show it on graph
+f_pol = lambda x: w[0] + w[1] * x
+x = np.arange(16)
+y = map(f_pol, x)
+pylab.plot(x, list(y))
+pylab.show()
+
+# show together : Investigation function and polynomial
+x = np.arange(16)
+y = map(f_pol, x)
+pylab.plot(x, list(y))
+y = map(f, x)
+pylab.plot(x, list(y))
+pylab.show()
