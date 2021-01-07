7ade1433a46bbf196f0ea1679168638cab434f23
zfima <zzfima@gmail.com>
2021-01-07 17:59:47 +0200

    normalize data

--- Supervised learning/Week1/assignment_1.py
+++ Supervised learning/Week1/assignment_1.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 
 advertising_data_frame = pd.read_csv('advertising.csv')
 
@@ -14,10 +12,28 @@ print(advertising_data_frame.shape, '\n')
 print(advertising_data_frame.describe(), '\n')
 
 # Show pairwise relationships as graphs between DataFrame features
-sns.pairplot(advertising_data_frame)
-plt.show()
+# sns.pairplot(advertising_data_frame)
+# plt.show()
 
 # Show pairwise relationships as correlations between DataFrame features
 print(advertising_data_frame.corr(), '\n')
 
-print('From graphs and correlation table can be seen, that TV most influences on Sales')
+print(
+    'From graphs and correlation table can be seen, that TV most influences on Sales - 78.2224% (linear relationship)')
+
+# Linear normalization to depression sigma: xi = (xi - mean(x)) / sigma
+# Long way:
+for column in advertising_data_frame:  # iterate each column
+    mean = advertising_data_frame[column].mean()  # Calculate mean of column
+    std = advertising_data_frame[column].std()  # Calculate std of column
+    print(column + ' parameters: mean=', mean, ', std=', std)
+    advertising_data_frame[column] = (advertising_data_frame[column] - mean) / std  # normalize each value in column
+
+# short way:
+# scaler = StandardScaler()
+# print(pd.DataFrame(scaler.fit_transform(advertising_data_frame), columns=advertising_data_frame.columns).head())
+
+print('\n')
+
+# Display first 5 rows
+print(advertising_data_frame.head(), '\n')
