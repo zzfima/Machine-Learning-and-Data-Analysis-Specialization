f6eab280de26c8d4675757edbe6febcf4c63378d
zfima <zzfima@gmail.com>
2021-01-07 17:15:17 +0200

    show conclusions

--- Supervised learning/Week1/assignment_1.py
+++ Supervised learning/Week1/assignment_1.py
@@ -13,6 +13,11 @@ print(advertising_data_frame.shape, '\n')
 # Display base statistics of DataFrame
 print(advertising_data_frame.describe(), '\n')
 
-# Show pairwise relationships
+# Show pairwise relationships as graphs between DataFrame features
 sns.pairplot(advertising_data_frame)
 plt.show()
+
+# Show pairwise relationships as correlations between DataFrame features
+print(advertising_data_frame.corr(), '\n')
+
+print('From graphs and correlation table can be seen, that TV most influences on Sales')
