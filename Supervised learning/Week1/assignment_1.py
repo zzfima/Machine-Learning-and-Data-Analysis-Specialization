a37d8201f28bf63f4a7be1f746590b3152dca147
zfima <zzfima@gmail.com>
2021-01-07 17:01:16 +0200

    start work

---
+++ Supervised learning/Week1/assignment_1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+advertising_data_frame = pd.read_csv('advertising.csv')
+
+# Display first 5 rows
+print(advertising_data_frame.head(), '\n')
+
+# Display size of DataFrame
+print(advertising_data_frame.shape, '\n')
+
+# Display base statistics of DataFrame
+print(advertising_data_frame.describe(), '\n')
